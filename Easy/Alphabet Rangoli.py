f425eae9fc0d5ddfa40c6845133fbd44801b2350
Aska Victoria <69489386+askazera@users.noreply.github.com>
2023-06-21 21:31:49 -0300

    Update Alphabet Rangoli.py

--- Easy/Alphabet Rangoli.py
+++ Easy/Alphabet Rangoli.py
@@ -1,5 +1,5 @@
 '''
-ou are given an integer, N . Your task is to print an alphabet rangoli of size N. (Rangoli is a form of Indian folk art based on creation of patterns.)
+You are given an integer, N . Your task is to print an alphabet rangoli of size N. (Rangoli is a form of Indian folk art based on creation of patterns.)
 
 Different sizes of alphabet rangoli are shown below:
 
